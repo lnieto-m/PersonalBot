555b7873e6905550bb32034886073051b950c9a2
Luis NIETO MUNOZ <lnieto-m@student.42.fr>
2022-05-08 12:37:25 +0200

    testing /get

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use('*', function(req, res, next) {
 app.options('*', cors());
 
 app.get('/getFanarts', async (req: Request, res: Response) => {
-    console.log('/getFanartsCalled', req.body);
+    console.log('/getFanartsCalled', req);
     const data = await getFanartWithDates(req.body);
     res.send(data);
 })
